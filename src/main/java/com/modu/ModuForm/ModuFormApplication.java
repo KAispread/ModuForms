d8b8556ceb3f5c6739ef912a398fb255e9a6cb55
KAispread <pain0121@naver.com>
2022-09-11 10:52:20 +0900

    Initial commit

---
+++ src/main/java/com/modu/ModuForm/ModuFormApplication.java
@@ -0,0 +1,13 @@
+package com.modu.ModuForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modu/ModuForm/ModuFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modu.ModuForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
