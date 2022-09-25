da115601e75632f377fc3a446d60e2719ec4a1cb
KAispread <pain0121@naver.com>
2022-09-25 12:45:29 +0900

    Cotroller 생성

---
+++ src/main/java/com/modu/ModuForm/app/web/UserController.java
@@ -0,0 +1,10 @@
+package com.modu.ModuForm.app.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+}
