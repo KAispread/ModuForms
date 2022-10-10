228b9d8aba09b343b97abcd460c9e48f0c852cf2
KAispread <pain0121@naver.com>
2022-10-10 23:32:54 +0900

    샘플 데이터 추가

--- src/main/java/com/modu/ModuForm/app/web/TestDataInit.java
+++ src/main/java/com/modu/ModuForm/app/web/TestDataInit.java
@@ -1,17 +1,15 @@
 package com.modu.ModuForm.app.web;
 
 import com.modu.ModuForm.app.domain.surbay.QuesType;
-import com.modu.ModuForm.app.domain.surbay.Survey;
 import com.modu.ModuForm.app.domain.surbay.SurveyQuestion;
 import com.modu.ModuForm.app.domain.surbay.SurveyRepository;
 import com.modu.ModuForm.app.domain.user.*;
-import com.modu.ModuForm.app.service.survey.SurveyService;
 import com.modu.ModuForm.app.web.dto.survey.SurveySaveDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,10 +19,10 @@ public class TestDataInit {
     private final UserRepository userRepository;
     private final AccessRepository accessRepository;
     private final SurveyRepository surveyRepository;
-    private final SurveyService surveyService;
 
     // 샘플 데이터 생성
     @PostConstruct
+    @Transactional
     public void init() {
         User user = userRepository.save(User.builder()
                 .name("기우")
@@ -32,7 +30,7 @@ public class TestDataInit {
                 .birth(19980101L)
                 .gender(Gender.MAN)
                 .email("asdf1234@naver.com")
-                .phone(0103213411L)
+                .phone(17635081L)
                 .role(Role.USER)
                 .company("AnyangUniv")
                 .build());
@@ -42,27 +40,26 @@ public class TestDataInit {
                 .password("love112")
                 .build());
 
-//        //given
-//        List<SurveyQuestion> surveyQuestionList = new ArrayList<>();
-//        surveyQuestionList.add(SurveyQuestion.builder()
-//                .number(1)
-//                .question("회식에 참여하십니까?")
-//                .questionType(QuesType.SHORT)
-//                .build());
-//        surveyQuestionList.add(SurveyQuestion.builder()
-//                .number(2)
-//                .question("참여자의 성함을 입력해주세요")
-//                .questionType(QuesType.SHORT)
-//                .build());
-//
-//        SurveySaveDto saveDto = SurveySaveDto.builder()
-//                .title("회식 참여 조사")
-//                .description("회식 참여 조사를 위한 설문입니다.")
-//                .deadLine("2022-10-06-15-30")
-//                .maximumAnswer(200)
-//                .surveyQuestionList(surveyQuestionList)
-//                .build();
-//
-//        Long saved = surveyService.save(saveDto, "Kai");
+        List<SurveyQuestion> surveyQuestionList = new ArrayList<>();
+        surveyQuestionList.add(SurveyQuestion.builder()
+                .number(0)
+                .question("회식에 참여하십니까?")
+                .questionType(QuesType.SHORT)
+                .build());
+        surveyQuestionList.add(SurveyQuestion.builder()
+                .number(1)
+                .question("참여자의 성함을 입력해주세요")
+                .questionType(QuesType.SHORT)
+                .build());
+
+        SurveySaveDto saveDto = SurveySaveDto.builder()
+                .title("회식 참여 조사")
+                .description("회식 참여 조사를 위한 설문입니다.")
+                .deadLine("2022-10-06-15-30")
+                .maximumAnswer(200)
+                .surveyQuestionList(surveyQuestionList)
+                .build();
+
+        surveyRepository.save(saveDto.toSurveyEntity(user));
     }
 }
