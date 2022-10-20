61ae90c3de946883c90543429f71b5ce1a7e6121
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-10-20 21:12:34 +0900

    로그아웃 확인 단계 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,8 +10,8 @@ let index = {
     },
     register : function () {
         let data = {
-            id : $('#input_id').val(),
-            pwd : $('#input_pwd').val(),
+            id : $('#id').val(),
+            pwd : $('#pwd').val(),
             username : $('#username').val(),
             nickname : $('#nickname').val(),
             birth : $('#birth').val(),
@@ -29,22 +29,25 @@ let index = {
             data: JSON.stringify(data)
         }).done(function () {
             alert('회원 가입이 완료되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
+            window.location.href = '/users/login';
+        }).fail(function () {
+            alert('올바른 정보를 입력해주세요.');
         })
     },
     logout : function () {
-        $.ajax({
-            type: 'POST',
-            url: '/app/users/logout',
-            dataType: 'json',
-        }).done(function (){
-            alert('로그아웃 했습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        })
+        if (window.confirm('로그아웃 하시겠습니까?'))
+        {
+            $.ajax({
+                type: 'POST',
+                url: '/app/users/logout',
+                dataType: 'json',
+            }).done(function (){
+                alert('로그아웃 했습니다.');
+                window.location.href = '/users/login';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            })
+        }
     }
 }
 
