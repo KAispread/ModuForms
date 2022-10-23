8591dc8c4046a46eee0b9f2651df37e2d40fdda8
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-10-24 00:33:23 +0900

    설문 응답 확인 API 추가

--- src/main/java/com/modu/ModuForm/app/web/controller/survey/SurveyIndexController.java
+++ src/main/java/com/modu/ModuForm/app/web/controller/survey/SurveyIndexController.java
@@ -2,7 +2,6 @@ package com.modu.ModuForm.app.web.controller.survey;
 
 import com.modu.ModuForm.app.domain.surbay.QuesType;
 import com.modu.ModuForm.app.service.survey.SurveyService;
-import com.modu.ModuForm.app.web.dto.survey.AnswerStringClass;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -20,40 +19,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SurveyIndexController {
     private final SurveyService surveyService;
     private final QuesType[] quesTypes = QuesType.values();
+
     @ModelAttribute("quesTypes")
     public QuesType[] quesTypes() {
         return quesTypes;
     }
 
-    @Operation(summary = "설문 등록 페이지", description = "설문을 등록하는 페이지입니다.")
+    @Operation(summary = "설문 등록 페이지", description = "설문을 등록하는 페이지를 반환합니다.")
     @GetMapping("/form")
     public String register(){
         return "survey/formTemplate";
     }
 
-    @Operation(summary = "설문 확인 페이지", description = "선택한 설문을 확인합니다.")
+    @Operation(summary = "설문 확인 페이지", description = "선택한 설문을 페이지를 반환합니다.")
     @GetMapping("/{id}")
     public String view(@PathVariable Long id, Model model) {
         model.addAttribute("surveyCheck", surveyService.getSurveyCheckDto(id));
         return "/survey/form-check";
     }
 
+    @Operation(summary = "설문 수정 페이지", description = "선택한 설문을 수정하는 페이지를 반환합니다.")
     @GetMapping("/{id}/edit")
     public String edit(@PathVariable Long id, Model model) {
         model.addAttribute("surveyCheck", surveyService.getSurveyCheckDto(id));
         return "/survey/form-edit";
     }
 
+    @Operation(summary = "설문 목록 페이지", description = "모든 설문을 수정하는 페이지를 반환합니다.")
     @GetMapping("/list")
     public String formList(Model model) {
         model.addAttribute("surveyPreviewList", surveyService.findAllOrderBy());
         return "/survey/formList";
     }
 
-    @GetMapping("/{id}/answer")
-    public String answerForm(@PathVariable Long id, Model model) {
-        model.addAttribute("surveyCheck", surveyService.getSurveyCheckDto(id));
-        model.addAttribute("answer", new AnswerStringClass());
-        return "/survey/form-answer";
+    @Operation(summary = "설문 응답 확인 페이지", description = "선택한 설문에 대한 응답을 확인하는 페이지를 반환합니다.")
+    @GetMapping("/{surveyId}/answer")
+    public String formResult(@PathVariable Long surveyId, Model model) {
+        model.addAttribute("queAnsList", surveyService.getSurveyAnswerCheckDto(surveyId));
+        return "/survey/form-check-Answer";
     }
 }
