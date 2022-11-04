ef210e645426b533b000e73ee17ab93a674d2546
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-04 20:07:11 +0900

    Feat: "OAuth2 인증 User 권한 수정"
    - OAuth 인가에 사용되는 권한(ROLE) 수정
    
    Related to: #20

--- src/main/java/com/modu/ModuForm/app/domain/user/Role.java
+++ src/main/java/com/modu/ModuForm/app/domain/user/Role.java
@@ -1,12 +1,11 @@
 package com.modu.ModuForm.app.domain.user;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public enum Role {
     USER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    GUEST("ROLE_GUEST", "손님");
 
     private final String key;
     private final String title;
@@ -14,13 +13,5 @@ public enum Role {
     Role(String key, String title) {
         this.key = key;
         this.title = title;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getTitle() {
-        return title;
     }
 }
