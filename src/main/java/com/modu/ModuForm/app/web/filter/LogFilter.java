aa570352a77e887619df4abbec2390e754f5c3b6
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-09 23:14:27 +0900

    feat: Log, 인증 기능 추가
    - Filter 를 이용하여 기능 구현
    - LogFilter -> 요청에 대한 Log 기록
    - LoginCheckFilter -> 세션을 통해 로그인된 사용자인지 판별

---
+++ src/main/java/com/modu/ModuForm/app/web/filter/LogFilter.java
@@ -0,0 +1,29 @@
+package com.modu.ModuForm.app.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("EXCEPTION : {}", e.getMessage());
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+}
---
+++ src/main/java/com/modu/ModuForm/app/web/filter/LoginCheckFilter.java
@@ -0,0 +1,47 @@
+package com.modu.ModuForm.app.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    private static final String[] whitelist = {"/users/login*", "/users/register*", "/app/users/login*", "/app/users/logout*", "/css/*", "/js/*", "/fonts/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            if (isLoginCheckPath(requestURI)) {
+                log.info("AUTHORIZE CHECK [{}]", requestURI);
+                HttpSession session = httpRequest.getSession(false);
+
+                if (session == null || session.getAttribute("user") == null) {
+                    log.warn("UNAUTHORIZED [{}]", requestURI);
+                    httpResponse.sendRedirect("/users/login?redirectURL=" + requestURI);
+                    return;
+                }
+            }
+
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("AUTHORIZE ERROR : {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    /* 화이트 리스트의 경우 인증 체크 X
+    * */
+    private boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+}
