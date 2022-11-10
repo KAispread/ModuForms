17de749ec47df12511bb1223d846433c7e9d10ff
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-10 23:10:50 +0900

    feat: 양방향 암호화 (AES256, TripleDES, RSA) 구현

---
+++ src/main/java/com/modu/ModuForm/app/web/config/auth/jwt/encrypt/AES256.java
@@ -0,0 +1,40 @@
+package com.modu.ModuForm.app.web.config.auth.jwt.encrypt;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Primary
+@Component
+public class AES256 implements Cryptography{
+    private final String alg = "AES/CBC/PKCS5Padding";
+    private final String key = "01234567890123456789012345678901";
+    private final String iv = key.substring(0, 16); // 16byte
+
+    @Override
+    public String encrypt(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    @Override
+    public String decrypt(String cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, "UTF-8");
+    }
+}
---
+++ src/main/java/com/modu/ModuForm/app/web/config/auth/jwt/encrypt/RSA.java
@@ -0,0 +1,66 @@
+package com.modu.ModuForm.app.web.config.auth.jwt.encrypt;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RSA {
+    /**
+     * 1024비트 RSA 키쌍을 생성
+     */
+    public static KeyPair genRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+        gen.initialize(1024, new SecureRandom());
+        return gen.genKeyPair();
+    }
+
+    /**
+     * Public Key로 RSA 암호화를 수행
+     */
+    public static String encryptRSA(String plainText, PublicKey publicKey)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] bytePlain = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(bytePlain);
+    }
+
+    /**
+     * Private Key로 RSA 복호화를 수행
+     */
+    public static String decryptRSA(String encrypted, PrivateKey privateKey)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        byte[] byteEncrypted = Base64.getDecoder().decode(encrypted.getBytes());
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytePlain = cipher.doFinal(byteEncrypted);
+        return new String(bytePlain, "utf-8");
+    }
+
+    public static PublicKey getPublicKeyFromBase64Encrypted(String base64PublicKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] decodedBase64PubKey = Base64.getDecoder().decode(base64PublicKey);
+
+        return KeyFactory.getInstance("RSA")
+                .generatePublic(new X509EncodedKeySpec(decodedBase64PubKey));
+    }
+
+    public static PrivateKey getPrivateKeyFromBase64Encrypted(String base64PrivateKey)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] decodedBase64PrivateKey = Base64.getDecoder().decode(base64PrivateKey);
+
+        return KeyFactory.getInstance("RSA")
+                .generatePrivate(new PKCS8EncodedKeySpec(decodedBase64PrivateKey));
+    }
+}
