a59f53f4281ccc9681bdf6caf0d03ba0e09e1eaa
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-10 23:08:24 +0900

    feat: 설정 정보에 Filter 추가

--- src/main/java/com/modu/ModuForm/app/web/config/WebConfig.java
+++ src/main/java/com/modu/ModuForm/app/web/config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.modu.ModuForm.app.web.config;
 
 import com.modu.ModuForm.app.web.config.auth.LoginUserArgumentResolver;
+import com.modu.ModuForm.app.web.filter.LogFilter;
+import com.modu.ModuForm.app.web.filter.LoginCheckFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Filter;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,5 +22,25 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterFilterRegistrationBean.setOrder(2);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
     }
 }
