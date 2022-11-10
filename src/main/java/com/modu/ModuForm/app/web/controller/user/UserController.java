8abc2d13e5bafe218e92fa097a183cdf6f35d988
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-10 23:32:00 +0900

    refactor: 로그인 이후 기존 페이지로 redirect 한다
    - 세션 생성 방식 수정
    - Thymeleaf: session 에서 유저 정보를 가져오는 부분 수정
    - 로그인 API 위치 변경

--- src/main/java/com/modu/ModuForm/app/web/controller/user/UserController.java
+++ src/main/java/com/modu/ModuForm/app/web/controller/user/UserController.java
@@ -1,17 +1,24 @@
 package com.modu.ModuForm.app.web.controller.user;
 
+import com.modu.ModuForm.app.domain.user.Access;
 import com.modu.ModuForm.app.service.user.UserServiceImpl;
+import com.modu.ModuForm.app.web.config.auth.SessionManager;
+import com.modu.ModuForm.app.web.dto.user.LoginRequestDto;
 import com.modu.ModuForm.app.web.dto.user.UserRegisterDto;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Api(tags = "User DATA handling API")
 @Slf4j
@@ -20,6 +27,7 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/app/users")
 public class UserController {
     private final UserServiceImpl userService;
+    private final SessionManager sessionManager;
 
     // 회원가입
     @Operation(summary = "회원가입", description = "회원가입 요청을 처리합니다.")
@@ -29,12 +37,30 @@ public class UserController {
         return userService.register(userRegisterDto);
     }
 
+    @Operation(summary = "로그인 요청 처리", description = "로그인 요청을 처리합니다.")
+    @PostMapping("/login")
+    public Long login(@Validated LoginRequestDto loginRequestDto, BindingResult bindingResult,
+                      @RequestParam(defaultValue = "/") String redirectURL,
+                      HttpServletResponse response) throws IOException {
+        if (bindingResult.hasErrors()) {
+            log.warn("바인딩 에러");
+            response.sendRedirect("/users/login");
+            return 0L;
+        }
+
+        Access loginAccess = userService.login(loginRequestDto);
+        log.info("{}: USER LOGIN", sessionManager.createSession(loginAccess));
+
+        response.sendRedirect(redirectURL);
+        return loginAccess.getId();
+    }
+
     @Operation(summary = "로그아웃 요청 처리", description = "로그아웃 요청을 처리합니다.")
     @PostMapping("/logout")
     public Long logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session == null) {
-            return 0L;
+            throw new SessionException("No Session available");
         }
 
         Long userPk = (Long) session.getAttribute("userPk");
--- src/main/java/com/modu/ModuForm/app/web/controller/user/UserIndexController.java
+++ src/main/java/com/modu/ModuForm/app/web/controller/user/UserIndexController.java
@@ -1,10 +1,7 @@
 package com.modu.ModuForm.app.web.controller.user;
 
-import com.modu.ModuForm.app.domain.user.Access;
 import com.modu.ModuForm.app.domain.user.Gender;
 import com.modu.ModuForm.app.service.user.UserService;
-import com.modu.ModuForm.app.web.config.SessionManager;
-import com.modu.ModuForm.app.web.dto.user.LoginRequestDto;
 import com.modu.ModuForm.app.web.dto.user.UserRegisterDto;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,14 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "User TEMPLATE handling")
 @Slf4j
@@ -28,29 +20,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class UserIndexController {
     private final UserService userService;
-    private final SessionManager sessionManager;
     private final Gender[] genders = Gender.values();
 
     @Operation(summary = "로그인 페이지", description = "로그인 페이지를 반환합니다.")
     @GetMapping("/login")
     public String userLogin() {
         return "loginForm";
-    }
-
-    @Operation(summary = "로그인 요청 처리", description = "로그인 요청을 처리합니다.")
-    @PostMapping("/login")
-    public String login(@Validated LoginRequestDto loginRequestDto, BindingResult bindingResult, HttpServletRequest request) {
-        if (bindingResult.hasErrors()) {
-            return "loginForm";
-        }
-        Access loginAccess = userService.login(loginRequestDto);
-        if (loginAccess == null) {
-            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return "loginForm";
-        }
-
-        log.info("{}: login application", sessionManager.createSession(request, loginAccess));
-        return "redirect:/";
     }
 
     @Operation(summary = "회원가입 페이지", description = "회원가입 페이지를 반환합니다.")
