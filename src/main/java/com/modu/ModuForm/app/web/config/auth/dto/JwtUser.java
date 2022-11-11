e8d0bba0beec6c1219167960e73c9194bb53f11f
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-11 23:15:10 +0900

    Feat: "JWT 인증 방식을 구현한다" (#20)
    *feat: JWT를 통해 User Dto 생성

---
+++ src/main/java/com/modu/ModuForm/app/web/config/auth/dto/JwtUser.java
@@ -0,0 +1,22 @@
+package com.modu.ModuForm.app.web.config.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class JwtUser {
+    private Long id;
+    private String name;
+    private String nickname;
+    private String email;
+
+    @Builder
+    public JwtUser(Long id, String name, String nickname, String email) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+    }
+}
