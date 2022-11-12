d05ff622ae39a22682e4f818cd55c869844e0ea2
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-13 00:18:56 +0900

    refactor: N+1 문제 -> EntityGraph 도입

--- src/main/java/com/modu/ModuForm/app/domain/surbay/SurveyRepository.java
+++ src/main/java/com/modu/ModuForm/app/domain/surbay/SurveyRepository.java
@@ -3,12 +3,16 @@ package com.modu.ModuForm.app.domain.surbay;
 import com.modu.ModuForm.app.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.FETCH;
+
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     List<Survey> findSurveysByUser(User user);
+    @EntityGraph(attributePaths = {"user", "answers"}, type = FETCH)
     Page<Survey> findAllByUser(User user, Pageable pageable);
     List<Survey> findAllByOrderByCreatedDateDesc();
 }
--- src/main/java/com/modu/ModuForm/app/domain/surbay/answer/AnswerRepository.java
+++ src/main/java/com/modu/ModuForm/app/domain/surbay/answer/AnswerRepository.java
@@ -4,11 +4,15 @@ import com.modu.ModuForm.app.domain.surbay.Survey;
 import com.modu.ModuForm.app.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.FETCH;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    @EntityGraph(attributePaths = {"user", "survey"}, type = FETCH)
     Page<Answer> findAllByUser(User user, Pageable pageable);
     void deleteAllBySurvey(Survey survey);
     List<Answer> findAllBySurvey(Survey survey);
