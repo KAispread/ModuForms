ee16aa71e759fd6039e1a1ad3e1639b7f89a4d08
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-17 23:18:49 +0900

    refactor: 메인 확인 페이지 기능 분리

---
+++ src/main/java/com/modu/ModuForm/app/service/MainPageService.java
@@ -0,0 +1,33 @@
+package com.modu.ModuForm.app.service;
+
+import com.modu.ModuForm.app.domain.surbay.Survey;
+import com.modu.ModuForm.app.domain.surbay.SurveyRepository;
+import com.modu.ModuForm.app.domain.surbay.answer.Answer;
+import com.modu.ModuForm.app.domain.surbay.answer.AnswerRepository;
+import com.modu.ModuForm.app.domain.user.User;
+import com.modu.ModuForm.app.domain.user.UserRepository;
+import com.modu.ModuForm.app.web.dto.user.UserFormDetailsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MainPageService {
+    private final UserRepository userRepository;
+    private final AnswerRepository answerRepository;
+    private final SurveyRepository surveyRepository;
+
+    @PerfLog
+    @Transactional(readOnly = true)
+    public UserFormDetailsDto getUserFormDetails(Pageable surveyPage, Integer currentSurveyPage,
+                                                 Pageable answerPage, Integer currentAnswerPage, Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+        Page<Survey> surveyPageList = surveyRepository.findAllByUser(user, surveyPage);
+        Page<Answer> answerPageList = answerRepository.findAllByUser(user, answerPage);
+
+        return new UserFormDetailsDto(user, surveyPageList, currentSurveyPage, answerPageList, currentAnswerPage);
+    }
+}
