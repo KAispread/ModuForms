614b8d54261221f9888feeb0153012523c223fc6
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-19 03:03:37 +0900

    refactor: 페이징 기능 분리

--- src/main/java/com/modu/ModuForm/app/service/user/UserService.java
+++ src/main/java/com/modu/ModuForm/app/service/user/UserService.java
@@ -1,13 +1,13 @@
 package com.modu.ModuForm.app.service.user;
 
 import com.modu.ModuForm.app.domain.user.Access;
-import com.modu.ModuForm.app.web.dto.user.*;
-import org.springframework.data.domain.Pageable;
+import com.modu.ModuForm.app.web.dto.user.LoginRequestDto;
+import com.modu.ModuForm.app.web.dto.user.UserDetailsDto;
+import com.modu.ModuForm.app.web.dto.user.UserRegisterDto;
 
 public interface UserService {
     Access login(LoginRequestDto loginRequestDto);
     Long register(UserRegisterDto userRequestDto);
-    UserFormDetailsDto getUserFormDetails(Pageable surveyPage, Integer currentSurveyPage, Pageable answerPage, Integer currentAnswerPage, Long id);
     UserDetailsDto getUserDetails(Long id);
     Long getUserPk(String nickName);
 }
