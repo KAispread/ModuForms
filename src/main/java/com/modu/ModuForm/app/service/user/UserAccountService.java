5ebf914bf916b2aa84270f21bf915fc243e43de5
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-27 16:19:54 +0900

    fix: 계정 삭제 기능 오류 수정
    - access 테이블에서 해당 User 을 참조하고 있어, 삭제 API 오류 발생
    - access 테이블도 삭제하도록 변경
    - orphanRemoval 속성 True 로 설정

--- src/main/java/com/modu/ModuForm/app/service/user/UserAccountService.java
+++ src/main/java/com/modu/ModuForm/app/service/user/UserAccountService.java
@@ -1,7 +1,7 @@
 package com.modu.ModuForm.app.service.user;
 
-import com.modu.ModuForm.app.domain.surbay.SurveyRepository;
 import com.modu.ModuForm.app.domain.surbay.answer.AnswerRepository;
+import com.modu.ModuForm.app.domain.user.AccessRepository;
 import com.modu.ModuForm.app.domain.user.User;
 import com.modu.ModuForm.app.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserAccountService {
     private final AnswerRepository answerRepository;
-    private final SurveyRepository surveyRepository;
     private final UserRepository userRepository;
+    private final AccessRepository accessRepository;
 
     @Transactional
     public void delete(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
         answerRepository.deleteAllByUser(user);
-        surveyRepository.deleteAllByUser(user);
+        accessRepository.deleteByUser(user);
         userRepository.delete(user);
     }
 }
