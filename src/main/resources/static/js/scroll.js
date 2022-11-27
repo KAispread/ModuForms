2c076b65e4ca4b511e7a55bb8838feb61829a911
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-28 01:29:12 +0900

    fix: 스크롤 오류 수정

--- src/main/resources/static/js/scroll.js
+++ src/main/resources/static/js/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let page_url = window.location.href;
     let page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
 
-    if (page_id == 'answer_point') {
+    if (page_id === 'answer_point') {
         $('html, body').animate({
             scrollTop: $('#section-divider').offset().top
         }, 500);
